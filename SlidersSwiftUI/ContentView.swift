b7dbddca17cae7a3cb0424040824d67642f7f72a
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-05 22:22:09 +0700

    Initial Commit

---
+++ SlidersSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidersSwiftUI
+//
+//  Created by Olga Yurchuk on 05.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidersSwiftUI/SlidersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUIApp.swift
+//  SlidersSwiftUI
+//
+//  Created by Olga Yurchuk on 05.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
